97ccbdf1ad050bc194efda0f66b9a3e3f75a9c92
@Samhubby <samayaasylumml@gmail.com>
2024-12-12 19:20:01 +0545

    add Tailwind CSS and Lottie animation support; configure PostCSS and create Asset component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import Asset from "./Asset";
 
 function App() {
   return (
-    <>
-      <h1 className="heading">Samaya Mahate</h1>
-    </>
+    <div className=" bg-gray-600 h-screen  flex items-center justify-center">
+      <div className="text-center space-y-3">
+        <h1 className="text-4xl text-white font-bold">Samaya Mahate</h1>
+        <h2 className="text-2xl text-white">Under Construction</h2>
+        <Asset />
+      </div>
+    </div>
   );
 }
 
---
+++ src/Asset.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const Asset = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/9e68ec9e-bfff-411b-a743-1858cb084a3b/dxAUWI0cbL.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+export default Asset
