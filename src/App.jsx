71467b32a6f8dbc9ee578fcb9070b2e94f51b75f
Samaya Mahate <101032666+Samhubby@users.noreply.github.com>
2025-03-30 21:14:39 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div className=" bg-gray-600 h-screen  flex items-center justify-center">
       <div className="text-center space-y-3">
         <h1 className="text-4xl text-white font-bold">Samaya Mahate</h1>
+         <h1 className="text-4xl text-white font-bold">I Love You Eshuuuuu</h1>
         <h2 className="text-2xl text-white">Under Construction</h2>
         <Asset />
       </div>
